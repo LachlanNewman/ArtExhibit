0c94ebec8f88baec1a0013d0a50f3db858c73c55
Ozojie Chikelu <ozojiechikelu@yahoo.com>
2017-10-17 09:19:36 +0100

    added test cases

---
+++ app/src/androidTest/java/com/lachlannewman/artexhibit/GalleryAdpaterTest.java
@@ -0,0 +1,10 @@
+package com.lachlannewman.artexhibit;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ozojiegerald on 10/17/17.
+ */
+
+public class GalleryAdpaterTest extends TestCase {
+}
